45ab339107ea5c4bde529f84ae329e474ce50f80
Callie Brazil <cbrazil@gmail.com>
2017-10-01 06:17:11 -0700

    Done.

--- js/app/services/MathService.js
+++ js/app/services/MathService.js
@@ -1,1 +1,11 @@
-// Your code here!
+function MathService() {
+  this.sum = function (arr) {
+    var sum = 0
+    arr.map(num => sum += num)
+    return sum
+  }
+}
+
+angular
+  .module('app')
+  .service('MathService', MathService);
