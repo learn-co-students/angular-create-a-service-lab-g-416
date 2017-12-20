dc6c2b5a8fff6af3dbca0e171788ccc6a10a68c3
Tyler <tbuchhei@alumni.nd.edu>
2017-12-19 23:55:44 -0600

    Done.

--- js/app/controllers/TotalController.js
+++ js/app/controllers/TotalController.js
@@ -1,13 +1,6 @@
 function TotalController(MathService) {
     this.sum = MathService.sum([
         2,
-        3,
-        4,
-        12,
-        23,
-        56,
-        73,
-        4,
         889
     ])
 }
